87c61727bc22c95e34a1daf4a997519df1b051dd
Gádor García <gador8@gmail.com>
2023-03-21 21:03:26 +0100

    Subida de este archivo

---
+++ remove_first_and_last.py
@@ -0,0 +1,33 @@
+"""
+lista = ['Rodrigo', 'Germán', 'Manuel', 'Hernán']
+números = list(range(1, 101))
+
+def remove_first_and_last(list_to_clean):
+  if type(list_to_clean) == list:
+    del list_to_clean[0], list_to_clean[-1]
+
+remove_first_and_last(números)
+
+print(números)
+"""
+
+#Solución Jordan
+
+def remove_first_and_last(list_to_clean):
+  if len(list_to_clean) >= 3: #Añadido por mí
+    _, *content, _ = list_to_clean
+    return content
+
+
+html = ['<h1>', 'My content', '</h1>']
+
+print(remove_first_and_last(html))
+
+other_content_to_clean = ['', 'My content', 'Something else', '/']
+
+print(remove_first_and_last(other_content_to_clean))
+
+#Añadido por mí
+not_enough = ['to clean']
+
+print(remove_first_and_last(not_enough))
