d06c714873ba62763360dc41a09d1da6a048db21
Gádor García <gador8@gmail.com>
2023-03-24 15:48:40 +0100

    subida archivo

---
+++ fizzbuzz_function.py
@@ -0,0 +1,13 @@
+def fizzbuzz(max_value):
+  for num in range(1, max_value):
+    if num % 3 == 0 and not num % 5 == 0:
+      print('Fizz')
+    elif num % 5 == 0 and not num % 3 == 0:
+      print('Buzz')
+    elif num % 3 == 0 and num % 5 == 0:
+      print('FizzBuzz')
+    else:
+      print(num)
+
+
+print(fizzbuzz(25))
