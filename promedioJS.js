c9d322a76c3c4782b7df6859ecac03c7b5de319a
Gdr18 <gador8@gmail.com>
2023-04-20 14:33:56 +0200

    Actualización archivo

--- promedioJS.js
+++ promedioJS.js
@@ -1,5 +1,15 @@
 var array = [1, 2, 3];
 result = (Number(array.slice(0, 1)) + Number(array.slice(1, 2)) + Number(array.slice(-1))) / array.length
 
-console.log(result)
+console.log(result);
 
+//Solución Jordan
+const getAverage = arr => {
+    const reducer = (total, currentValue) => total + currentValue;
+    const total = arr.reduce(reducer);
+    return total / arr.length;
+  };
+  
+  const arr = [1, 2, 3, 4, 5, 6];
+  
+  getAverage(arr);
