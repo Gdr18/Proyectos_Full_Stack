6b47089f171d7ed4d5fde7137a72458857d5f3fd
Gdr18 <gador8@gmail.com>
2023-04-27 12:44:10 +0200

    Actualización archivo

--- htmlHeadingGenerator.js
+++ htmlHeadingGenerator.js
@@ -1,5 +1,8 @@
 const htmlHeadingGenerator = (title, heading) => {
-    console.log(`<h${heading}>${title}<h${heading}>`);
+    console.log(`<h${heading}>${title}</h${heading}>`);
 }
 
 htmlHeadingGenerator('Hello World', 2);
+
+
+//Solución Jordan igual que la mía.
