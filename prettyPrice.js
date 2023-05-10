11305cac29c7372f07d4aef134897b7226b37ef2
Gdr18 <gador8@gmail.com>
2023-05-10 13:39:35 +0200

    Actualización archivo

--- prettyPrice.js
+++ prettyPrice.js
@@ -4,3 +4,18 @@ const prettyPrice = (total, extension) => {
 
 
 console.log(prettyPrice(3.45, 95));
+
+
+/*Solución Jordan:
+const prettyPrice = (grossPrice, extension) => {
+  if (Number.isInteger(extension)) {
+    extension = extension * 0.01;
+  }
+  return Math.floor(grossPrice) + extension;
+};
+
+prettyPrice(2.2, 0.95);
+prettyPrice(2.2, 95);
+prettyPrice(2.2, 0); 
+prettyPrice(2.2, 99);
+*/
